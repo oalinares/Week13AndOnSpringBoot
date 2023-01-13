48ff4538590c9c364fdc12b4b484ff560339e7f8
unknown <oalinares.jr@gmail.com>
2023-01-13 13:50:00 -0600

    Commit for Week 15 assignments

--- src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
+++ src/test/java/com/promineotech/jeep/controller/support/FetchJeepTestSupport.java
@@ -1,8 +1,12 @@
 package com.promineotech.jeep.controller.support;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
@@ -28,7 +32,21 @@ public class FetchJeepTestSupport extends BaseTest {
       
       //@formatter:on
  
-      
+      Collections.sort(list);
       return list;
     }
+    /**
+     * 
+     * @param error
+     * @param status
+     */
+    protected void assertErrorMessageValid(Map<String, Object> error, HttpStatus status) {
+      assertThat(error)
+      .containsKey("message")
+      .containsEntry("status code", status.value())
+      .containsEntry("uri", "/jeeps")
+      .containsKey("timestamp")
+      .containsEntry("reason", status.getReasonPhrase());
+    }
+   
 }
